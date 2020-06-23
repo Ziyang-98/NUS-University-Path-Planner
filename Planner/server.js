54a90c28771cafbf981e98db504143f8185c031b
siangernlow <65005806+siangernlow@users.noreply.github.com>
2020-06-23 19:38:15 +0800

    Add files via upload

--- Planner/server.js
+++ Planner/server.js
@@ -16,6 +16,8 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
         //new
         const data = db.collection('data')
 
+        let curr_user = ""
+
         app.use(bodyParser.urlencoded({ extended: true }))
         app.use(bodyParser.json())
         app.use('/', router)
@@ -63,47 +65,76 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
                             const year = '20' + data.AY.slice(0, 2) + '-20' + data.AY.slice(3, 5)
                             if (!results) {   //if modules document not initialised yet                               
                                 const modules = {}
-                                //fetch the data from the corresponding year
-                                fetch(`https://api.nusmods.com/v2/${year}/moduleList.json`)
+                                fetch(`https://api.nusmods.com/v2/${year}/moduleInfo.json`)
                                     .then(response => response.json())
                                     .then(data => {
-                                        const moduleList = {}
-                                        for (module of data) {
-                                            const name = module.moduleCode
-                                            moduleList[name] = module
+                                        //REPLACE THE INITIALISATION WITH THE FOLLOWING CODE:
+                                        const moduleList = {
+                                            1: {},
+                                            2: {},
+                                            3: {},
+                                            4: {}
                                         }
+                                        for (module of data) {
+                                            const semesterData = module.semesterData
+                                            const moduleData = {
+                                                moduleCode: module.moduleCode,
+                                                title: module.title,
+                                                moduleCredit: module.moduleCredit
+                                            }
+                                            for (sem of semesterData) {
+                                                moduleList[sem.semester][module.moduleCode] = moduleData
+                                            }
+                                        }
+                                        //TILL HERE. REPEAT FOR THE ELSE IF STATEMENT BELOW
                                         modules[AY] = moduleList
-                                        console.log(Object.keys(modules))
                                         db.collection('data').insertOne({ modules: modules })
                                     })
-                            } else if (!results['modules'].hasOwnProperty(AY)) { //no module list for current year
+                            } else if (!results['modules'].hasOwnProperty(data.AY)) { //no module list for current year
                                 const modules = results.modules
                                 //fetch the data from the corresponding year
-                                fetch(`https://api.nusmods.com/v2/${year}/moduleList.json`)
+                                fetch(`https://api.nusmods.com/v2/${year}/moduleInfo.json`)
                                     .then(response => response.json())
                                     .then(data => {
-                                        const moduleList = {}
+                                        const moduleList = {
+                                            1: {},
+                                            2: {},
+                                            3: {},
+                                            4: {}
+                                        }
                                         for (module of data) {
-                                            const name = module.moduleCode
-                                            moduleList[name] = module
+                                            const semesterData = module.semesterData
+                                            const moduleData = {
+                                                moduleCode: module.moduleCode,
+                                                title: module.title,
+                                                moduleCredit: module.moduleCredit
+                                            }
+                                            for (sem of semesterData) {
+                                                moduleList[sem.semester][module.moduleCode] = moduleData
+                                            }
                                         }
                                         modules[AY] = moduleList
                                         db.collection('data').updateOne({ modules: { $exists: true } }, { $set: { modules: modules } })
                                     })
                             }
                         })
-                        .then(ignore => {  //no input from prev 
-                            res.render('index.ejs', {
-                                name: req.params.name,
-                                modules: data.moduleList,
-                                AY: AY,
-                                semester: semester
-                            })
-                        })
+                    return {
+                        moduleList: data.moduleList
+                    }
+                })
+                .then(data => {
+                    res.render('index.ejs', {
+                        name: req.params.name,
+                        modules: data.moduleList,
+                        AY: AY,
+                        semester: semester
+                    })
                 })
         })
 
+
         app.post('/name', (req, res) => {
+            curr_user = req.body.name
             db.collection('data').findOne({ users: { $exists: true } })
                 .then(results => {
                     //if database is not initialised yet
@@ -122,9 +153,11 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
                         }
                         results['users'][req.body.name] = user
                         db.collection('data').updateOne({ users: { $exists: true } }, { $set: results })
+                            .then(res.redirect(`/users/${req.body.name}`))
+                    } else {
+                        res.redirect(`/users/${req.body.name}`)
                     }
                 })
-                .then(res.redirect(`/users/${req.body.name}`))
                 .catch(err => console.error(err))
         })
 
@@ -196,7 +229,7 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
                         results.users[req.params.name] = user
                         db.collection('data').updateOne({ users: { $exists: true } }, { $set: { users: results.users } })
                     }
-                    
+
                     res.render('index.ejs', {
                         name: req.body.name,
                         modules: moduleList,
@@ -205,6 +238,125 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
                     })
                 })
                 .catch(err => console.error(err))
+        })
+
+        app.get('/reviews', (req, res) => {
+            db.collection('data').findOne({ reviews: { $exists: true } })
+                .then(results => {
+                    res.render('reviews.ejs', {
+                        reviews: results.reviews
+                    })
+                })
+        })
+
+        app.get('/reviews/:name', (req, res) => {
+            db.collection('data').findOne({ reviews: { $exists: true } })
+                .then(results => {
+                    reviews = results.reviews
+                    const review = reviews[req.params.name]
+                    res.render('review-template.ejs', {
+                        review: review
+                    })
+                })
+        })
+
+        app.post('/upvote', (req, res) => {
+            db.collection('data').findOne({ reviews: { $exists: true } })
+                .then(results => {
+                    reviews = results.reviews
+                    const review = reviews[req.body.name]
+
+                    //logic of upvoting
+                    const upvoted = JSON.parse(req.body.upvoted).filter(user => user === curr_user)
+                    const downvoted = JSON.parse(req.body.downvoted).filter(user => user === curr_user)
+
+                    //if user has previously downvoted
+                    if (downvoted.length > 0) {
+                        review.votes = review.votes + 1
+                        review.downvoted = JSON.parse(req.body.downvoted).filter(user => user != curr_user)
+                    } else if (upvoted.length == 0) {
+                        review.votes = review.votes + 1
+                        review.upvoted.push(curr_user)
+                    }
+
+                    reviews[req.body.name] = review
+                    results.reviews = reviews
+                    db.collection('data').updateOne({ reviews: { $exists: true } }, { $set: { reviews: results.reviews } })
+                })
+                .then(ignore => {
+                    res.redirect(`/reviews/${req.body.name}`)
+                })
+        })
+
+        app.post('/downvote', (req, res) => {
+            db.collection('data').findOne({ reviews: { $exists: true } })
+                .then(results => {
+                    reviews = results.reviews
+                    const review = reviews[req.body.name]
+
+                    //logic of downvoting
+                    const upvoted = JSON.parse(req.body.upvoted).filter(user => user === curr_user)
+                    const downvoted = JSON.parse(req.body.downvoted).filter(user => user === curr_user)
+
+                    if (upvoted.length > 0) {
+                        review.votes = review.votes - 1
+                        review.upvoted = JSON.parse(req.body.upvoted).filter(user => user != curr_user)
+                    } else if (downvoted.length == 0) {
+                        review.votes = review.votes - 1
+                        review.downvoted.push(curr_user)
+
+                    }
+                    reviews[req.body.name] = review
+                    results.reviews = reviews
+                    db.collection('data').updateOne({ reviews: { $exists: true } }, { $set: { reviews: results.reviews } })
+                })
+                .then(ignore => {
+                    res.redirect(`/reviews/${req.body.name}`)
+                })
+        })
+
+        app.post('/reviews/:name', (req, res) => {
+            const review = {
+                name: req.body.name,
+                moduleList: JSON.parse(req.body.moduleList),
+                title: req.body.title,
+                desc: req.body.desc,
+                votes: req.body.votes,
+                upvoted: JSON.parse(req.body.upvoted),
+                downvoted: JSON.parse(req.body.downvoted)
+            }
+            res.render('review-template.ejs', {
+                review: review
+            })
+        })
+
+        app.post('/submit-review', (req, res) => {
+            res.render('submit-review.ejs', {
+                name: req.body.name,
+                moduleList: JSON.parse(req.body.moduleList)
+            })
+        })
+
+        app.post('/reviews', (req, res) => {
+            db.collection('data').findOne({ reviews: { $exists: true } })
+                .then(results => {
+                    reviews = results.reviews
+                    const review = {
+                        name: req.body.name,
+                        moduleList: JSON.parse(req.body.moduleList),
+                        title: req.body.title,
+                        desc: req.body.desc,
+                        votes: 0,
+                        upvoted: [],
+                        downvoted: []
+                    }
+                    reviews[req.body.name] = review
+                    results.reviews = reviews
+                    db.collection('data').updateOne({ reviews: { $exists: true } }, { $set: { reviews: results.reviews } })
+                })
+                .then(ignore => {
+                    res.redirect('/reviews')
+                })
         })
 
         app.listen(4000, () => {
